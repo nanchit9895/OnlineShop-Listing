31b27110f82071b79a3ec809e0fb2bd9e98ed5db
sabaiaungmhs <sabai.aung@globalway.co.jp>
2020-04-07 15:00:39 +0630

    Open My Shop

---
+++ src/views/OpenMyShop.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="content-wrapper">
+        <div class="container-fluid">
+            <div class="openmyshopTitle">
+                မိမိ Shop တင်မည်
+            </div>
+        </div>
+    </div>
+</template>
