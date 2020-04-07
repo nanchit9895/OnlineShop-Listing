1beb977acf19c95805f1a0f862a0f52bef0d8500
minnkhantwunna <minnkhantwunna.mhs@gmail.com>
2020-04-07 10:06:59 +0630

    'change-bootstrap'

---
+++ src/views/ShopSearch1.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="content-wrapper">
+        <header>
+            <div class="prev-arrow">
+                <img src="../../public/images/productsearch/Asset 28.png" alt="previousArrow" class="prevArrow"/>
+            </div>
+        </header>
+        
+    </div>
+</template>
+
+<style scoped>
+
+ @import "../../public/css/shopsearch1.css"
+
+</style>
